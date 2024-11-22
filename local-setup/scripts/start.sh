bfbb8a36379e3e1dbe5795a0f8f6713fd0e7cf42
Bastian Echterhölter <bastian.echterhoelter@sap.com>
2024-11-22 10:34:55 +0100

    feat: moving aspects of the local setup to helm-charts. this is not the final state but a preparation step

---
+++ local-setup/scripts/start.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+set -e
+COL='\033[92m'
+COL_RES='\033[0m'
+# argument to select the installation flavor, options are all or min, verify if the argument was provided
+if [ "${1}" != "all" ] && [ "${1}" != "remote" ]; then
+    echo "Invalid installation flavor provided, please provide either all (start.sh all) or min (start.sh min)"
+    exit 1
+fi
+
+# Check for input argument GH_TOKEN and echo message in case not provided
+if [ -z "${GH_TOKEN}" ]; then
+    echo "Please provide the GitHub Token as an environment variable"
+    exit 1
+fi
+
+# Check if kind cluster kind is already running, exit if yes
+if [ $(kind get clusters | grep -c openmfp) -gt 0 ]; then
+    echo -e "$COL Kind cluster already running, using existing $COL_RES"
+    kind export kubeconfig --name openmfp
+else
+  echo -e "$COL Creating kind cluster $COL_RES"
+  kind create cluster --config kind/kind-config.yaml --name openmfp --image=kindest/node:v1.30.2
+fi
+
+# Install flux
+echo "$COL Installing flux $COL_RES"
+flux install --components source-controller,helm-controller
+
+# Prepare installation namespace
+echo "$COL Creating openmfp-system namespace $COL_RES"
+kubectl apply -k infrastructure/namespace
+
+echo "$COL Creating necessary secrets $COL_RES"
+flux create secret oci ghcr-credentials -n openmfp-system --url ghcr.io --username $(gh api user | jq -r '.login') --password $GH_TOKEN
+kubectl create secret generic keycloak-admin -n openmfp-system --from-literal=secret=admin --dry-run=client -o yaml | kubectl apply -f -
+
+
+echo "$COL starting deployments $COL_RES"
+kubectl apply -k flavors/local-${1}
+
+echo "$COL waiting for istio to become ready $COL_RES"
+kubectl wait --namespace istio-system \
+  --for=condition=Ready helmreleases \
+  --timeout=120s istio-base
+
+kubectl wait --namespace istio-system \
+  --for=condition=Ready helmreleases \
+  --timeout=120s istio-istiod
+
+kubectl wait --namespace istio-system \
+  --for=condition=Ready helmreleases \
+  --timeout=120s istio-gateway
+
+echo "$COL waiting for openmfp to become ready $COL_RES"
+
+kubectl wait --namespace openmfp-system \
+  --for=condition=Ready helmreleases \
+  --timeout=120s openmfp-crds
+
+kubectl wait --namespace openmfp-system \
+  --for=condition=Ready helmreleases \
+  --timeout=120s openmfp
+
+echo "You can access http://localhost:8000 to access the portal"
+
