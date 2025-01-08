04b73ad4a7eee87b8aa4c2161c463d7a98b01378
Angel Kafazov <akafazov@cst-bg.net>
2025-01-08 14:59:06 +0200

    fix: increase timeout for helmrelease readiness check in start.sh
    On-behalf-of: @SAP angel.kafazov@sap.com
    Signed-off-by: Angel Kafazov <akafazov@cst-bg.net>

--- local-setup/scripts/start.sh
+++ local-setup/scripts/start.sh
@@ -79,7 +79,7 @@ kubectl wait --namespace openmfp-system \
 
 kubectl wait --namespace openmfp-system \
   --for=condition=Ready helmreleases \
-  --timeout=1500s openmfp
+  --timeout=2500s openmfp
 
 echo "-------------------------------------"
 echo "Installation Complete ♥!"
