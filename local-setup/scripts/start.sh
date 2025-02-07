5bbcc1f3d71e7a437df8eed052253d4e2aacdd1c
Angel Kafazov <akafazov@cst-bg.net>
2025-02-07 18:07:54 +0200

    fix (arch): apply keycloak patch for arm64
    On-behalf-of: @SAP angel.kafazov@sap.com
    Signed-off-by: Angel Kafazov <akafazov@cst-bg.net>

--- local-setup/scripts/start.sh
+++ local-setup/scripts/start.sh
@@ -53,6 +53,11 @@ helm upgrade -i -n flux-system --create-namespace flux oci://ghcr.io/fluxcd-comm
 echo -e "${COL}[$(date '+%H:%M:%S')] Starting deployments ${COL_RES}"
 kubectl apply -k $SCRIPT_DIR/../kustomize/overlays/default
 
+if [[ "$(uname -m)" == "arm64" ]]; then
+  echo -e "${COL}[$(date '+%H:%M:%S')] ARM64 architecture detected, applying patch ${COL_RES}"
+  kubectl apply -k $SCRIPT_DIR/../kustomize/overlays/arm64
+fi
+
 if [ "${1}" == "oci" ]; then
   kubectl apply -k $SCRIPT_DIR/../kustomize/overlays/oci
   sleep 10 # give time for the 'registry' pod to be created
